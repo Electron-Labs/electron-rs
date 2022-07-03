359af5527d2a7a6dc10b360fbe6876cb6aad34ca
Jinank Jain <jinank94@gmail.com>
2022-07-03 13:24:34 +0530

    src: Remove boilerplate code generated by cargo
    There is no need of the boilerplate generated by cargo. So let's just
    remove it.
    
    Signed-off-by: Jinank Jain <jinank94@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,3 @@
 // Copyright © 2022, Electron Labs
 
 pub mod verifier;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
